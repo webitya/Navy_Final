5725d19b550de82c78d7ae65bc572dcf172f765e
Webitya <webitya@gmail.com>
2024-10-28 12:28:28 +0530

    adiiiiii

--- new2/vite.config.js
+++ new2/vite.config.js
@@ -1,15 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: "https://navy-final.onrender.com",
+        target: 'https://navy-final.onrender.com',
         changeOrigin: true,
+        secure: false,
       },
     },
   },
-})
+  middleware: () => {
+    return createProxyMiddleware('/api', {
+      target: 'https://navy-final.onrender.com',
+      changeOrigin: true,
+      secure: false,
+});
+},
+});
