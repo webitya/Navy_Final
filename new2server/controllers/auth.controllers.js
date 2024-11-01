1d65ec265e5273d29faf219be40b3e6ad03bce32
BEaANIKET <aniketchaturvedi309@gmail.com>
2024-11-01 14:14:14 +0530

    resolve error 5 Aniket

--- new2server/controllers/auth.controllers.js
+++ new2server/controllers/auth.controllers.js
@@ -28,10 +28,9 @@ export const Login = async (req, res) => {
         return res
             .status(200)
             .cookie('token', token, {
+                expires: new Date(Date.now() + 86400000), // 1 day in milliseconds
                 httpOnly: true,
                 secure: process.env.NODE_ENV === 'production',
-                sameSite: 'Lax',
-                maxAge: 24 * 60 * 60 * 1000,
             })
             .json({
                 message: 'Login successful',
