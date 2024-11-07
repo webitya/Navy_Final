c0fcb011ea0ed2685c843ee8e7c04ff0f5222322
adityawintech <aditya#19AI>
2024-11-07 07:51:27 +0530

    Aditya

--- new2/src/ElementsEl/ContactEl/constactUs2/index.jsx
+++ new2/src/ElementsEl/ContactEl/constactUs2/index.jsx
@@ -109,7 +109,7 @@ const ContactUs = () => {
           <Title level={2} className="text-3xl font-bold text-blue-500 mb-4 text-center">Find Us Here!</Title>
           <iframe
             title="Google Maps"
-            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3659.546430397639!2d85.46555337532754!3d23.47681987885798!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjPCsDI4JzM2LjYiTiA4NcKwMjgnMDUuMyJF!5e0!3m2!1sbn!2sin!4v1729413740646!5m2!1sbn!2sin"
+            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3662.8539407654944!2d85.2753333!3d23.3573056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjPCsDIxJzI2LjMiTiA4NcKwMTYnMzEuMiJF!5e0!3m2!1sen!2sin!4v1730946006188!5m2!1sen!2sin"
             width="100%"
             height="400"
             style={{ border: 0 }}
